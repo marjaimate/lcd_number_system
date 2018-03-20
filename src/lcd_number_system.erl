5eae468e5582e957e74ef4ac65f32607ec01a428
Máté Marjai <mate@theproduct.works>
2018-03-20 20:10:35 +0000

    Added comments.

--- src/lcd_number_system.erl
+++ src/lcd_number_system.erl
@@ -22,7 +22,10 @@
 %% which translates the numbers:
 %% 1 to 000, 001, 001
 %% 2 to 010, 011, 110
+%% etc.
 %%
+%% we'll represent these as binary 0s and 1s, with triplets of data (see above)
+%% where the first and third are pipes, the middle is an underscore
 %%
 -define(LCD, #{
             $0 => <<"010101111">>,
@@ -42,8 +45,9 @@ to_lcd(N) ->
     %% [[[" "," "," "],[" "," ","|"],[" "," ","|"]],
     %%  [[" ","_"," "],["|"," ","|"],["|","_","|"]]]
     %%
-    %%  we need to turn this into rows:
+    %%  we need to turn this into rows of strings
     Defs = to_lcd(N, []),
+
     Row1 = get_row(Defs, 1),
     Row2 = get_row(Defs, 2),
     Row3 = get_row(Defs, 3),
@@ -57,11 +61,13 @@ number_to_lcd(Char) ->
     Def = maps:get(Char, ?LCD),
     lcd_to_string(Def).
 
+%% Turn the crazy binary to something more meaningful
 lcd_to_string(S) -> lcd_to_string(S, []).
 lcd_to_string(<<>>, Str) -> Str;
 lcd_to_string(<<Pipe1:1/binary, US:1/binary, Pipe2:1/binary, Rest/binary>>, Str) ->
     lcd_to_string(Rest, Str ++ [ [pipe(Pipe1), underscore(US), pipe(Pipe2)] ]).
 
+%% Pipe it up
 pipe(<<"0">>) -> " ";
 pipe(<<"1">>) -> "|".
 underscore(<<"0">>) -> " ";
